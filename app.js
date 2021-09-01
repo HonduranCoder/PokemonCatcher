876e0e563fdaa2fa96e41b6cbd1cdd11508d15e2
Estefani <stef45689@gmail.com>
2021-09-01 17:39:18 -0500

    Functions working, Local Storage Working. Layout for Results Added.

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 // import functions and grab DOM elements
+import { renderNewPokemon } from './render-utils.js';
+import { catchPokemon, encounterPokemon } from './localstorage.utils.js';
+import { getRandomPokemon } from './data-utils.js';
+
+const button = document.getElementById('pressgo');
 
 // initialize global state
-
+let pokemonCaught = 0;
 // set event listeners 
   // get user input
   // use user input to update state 
@@ -10,11 +15,33 @@
 /*User loads the page
 
 call renderNewPokemon()
-User clicks catch button
+User clicks catch button */
 
-increment: pokeCaught++
+const randomArray = getRandomPokemon();
+
+renderNewPokemon(randomArray);
+
+/* increment: pokeCaught++
 We figure out the id of the pokemon that was captured.
 call catchPokemon(id) with this id
 now, if pokeCaught > 10, redirect to the results page
 call renderNewPokemon()
 */
+
+button.addEventListener('click', () => {
+    const selectedRadio = document.querySelector('input:checked');
+    const pokemonDiv = document.getElementById('pokemon-div');
+    const chosenPokemon = Number(selectedRadio.value);
+    pokemonDiv.textContent='';
+    pokemonCaught++;
+    catchPokemon(chosenPokemon);
+
+    if (pokemonCaught < 10) {
+        const randomArray = getRandomPokemon(); 
+        renderNewPokemon(randomArray);
+    } else {
+     /* window.location.href = 'index.html'; */ /*goes to results page*/
+    }
+  
+});
+  
--- localstorage.utils.js
+++ localstorage.utils.js
@@ -3,3 +3,66 @@
 let pokeCaught = 0
 STRETCH: we could derive this from our other data, just counting how many total pokemon the user has already caught: howManyCaughtSoFar()
 */
+
+import { findById } from './utils.js';
+
+export const CATCH = 'CATCH'; 
+export const POKEMON = 'POKEMON'; 
+
+// setPokedex(pokedex)
+// - takes in a pokedex, stringifies it and puts it into local storage
+export function setPokedex(pokemonArray){
+    const stringyPokemon = JSON.stringify(pokemonArray); 
+    localStorage.setItem(POKEMON, stringyPokemon);
+}
+
+// getPokedex()
+// - retrieves and parses the pokedex in local storage
+export function getPokedex(){
+    const stringyPokemon = localStorage.getItem(POKEMON);
+    if (!stringyPokemon){
+        return [];
+    }
+    const finalPokemon = JSON.parse(stringyPokemon); 
+    return finalPokemon;
+}
+
+// encounterPokemon(id)
+// - getPokedex
+// - If the pokemon has been previously seen, just increment the times seen
+// - If this is the first time, make a new object with `{ id: 5, encoutered: 1, caught: 0 }`
+// - setPokedex
+
+export function encounterPokemon(id){
+    const pokedex = getPokedex();
+    const pokedexMatch = findById(pokedex, id); 
+    //conditional statment
+    if (pokedexMatch){
+        pokedexMatch.encountered++;
+    } else {
+        const pokedexNew = {
+            id: id, 
+            captured: 0,
+            encountered: 1,
+        };
+        pokedex.push(pokedexNew);
+    }
+    setPokedex(pokedex);
+}
+
+// catchPokemon(id)
+// - getPokedex
+// - no need to check if it's been encountered. If you got this far, it _has_ been encountered.
+// - Increment the `caught` of this pokemon in local storage
+// - setPokedex
+export function catchPokemon(id){
+    const pokedex = getPokedex();
+    const pokedexMatch = findById(pokedex, id); 
+    //increment caught 
+    pokedexMatch.captured++;
+    setPokedex(pokedex);
+}
+
+
+
+
